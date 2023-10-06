cd53737d6a98891cad4be8066f2dd166044a1884
Kalden14 <badmomba0414@gmail.com>
2023-10-06 13:01:04 -0500

    added player movement

--- Our Game Unity/Assets/Scripts/PlayerMovement.cs
+++ Our Game Unity/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed = 5.0f;
+    public float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +17,13 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
