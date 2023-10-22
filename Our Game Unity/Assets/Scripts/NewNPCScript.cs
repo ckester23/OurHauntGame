aa0d02d0a61b7ee32b3f297559c0c6ddfe0aefcf
Cheyanne Kester <fearlesspunkin@gmail.com>
2023-10-22 18:09:27 -0500

    Added new NPC prefab
    now has a decent wander and flee

--- Our Game Unity/Assets/Scripts/NewNPCScript.cs
+++ Our Game Unity/Assets/Scripts/NewNPCScript.cs
@@ -7,22 +7,22 @@ public class NewNPCScript : MonoBehaviour
 {
     public float wanderSpeed = 2f;
     public float fleeSpeed = 4f;
-    public float fleeDistance = 5f;
+
+    public float fleeDistance = 3f;
     public float visionDistance = 10f;
-    /*public LayerMask itemLayer;
-    public LayerMask obstacleLayer;
-    */
+
+    public int fearLevel = 100;
 
     public Transform waypoint;
 
     private Transform center;
     private Transform exit;
     private Transform player;
-    
+    private NavMeshAgent agent;
+
     private bool isFleeing = false;
     private Vector3 wanderTarget;
-    
-    private NavMeshAgent agent;
+
 
     // Start is called before the first frame update
     void Start()
@@ -36,33 +36,15 @@ public class NewNPCScript : MonoBehaviour
         // Disabling auto-braking allows for continuous movement
         // between points (ie, the agent doesn't slow down as it
         // approaches a destination point).
+
+        agent.speed = wanderSpeed;
+
         agent.autoBraking = false;
 
-        GotoNextPoint();
+        GenerateNewWaypoint();
     }
 
-
-    void GotoNextPoint()
-    {
-        // if NPC is fleeing, don't reset the waypoint (shouldn't ever need to be in this section)
-        if (isFleeing)
-            return;
-
-        // select a destination
-        GenerateNewWaypoint(10f);
-
-        // Set the agent to go to the destination.
-        agent.destination = waypoint.position;
-
-    }
-
-    void Flee()
-    {
-        // select a point towards the exit, away from player
-        GenerateNewWaypoint(0f);
-    }
-
-    void GenerateNewWaypoint(float range)
+     void GenerateNewWaypoint()
     {
         float wayX;
         float wayZ;
@@ -73,7 +55,7 @@ public class NewNPCScript : MonoBehaviour
             // cap this, so they don't just leave every time they get spooked
             // if player does an ultimate scare, they do leave
             wanderTarget = new Vector3(exit.position.x, transform.position.y, exit.position.z);
-
+            agent.speed = fleeSpeed;
         }
         else
         {
@@ -83,9 +65,10 @@ public class NewNPCScript : MonoBehaviour
             //wayX = Random.Range(-1f*range, range);
             //wayZ = Random.Range(-1f * range, range);
             wanderTarget = new Vector3(center.position.x + wayX, transform.position.y, center.position.z + wayZ);
-
+            agent.speed = wanderSpeed;
         }
         waypoint.position = wanderTarget;
+        agent.destination = waypoint.position;
     }
 
 
@@ -95,13 +78,17 @@ public class NewNPCScript : MonoBehaviour
         // close to the current one.
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
         {
-            //Debug.Log("I'm getting close!");
+            GenerateNewWaypoint();
             if (isFleeing) isFleeing = !isFleeing;
-            GotoNextPoint();
         }
 
-        // TODO Check where the player is
-        // if too close, flee
-            
+        // TODO Edit to happen when scared, not when player is too close
+        // if player is too close, flee
+        float distToPlayer = Vector3.Distance(player.position, transform.position);
+        if (distToPlayer < fleeDistance)
+        {
+            isFleeing = true;
+            GenerateNewWaypoint();
+        }
     }
 }
